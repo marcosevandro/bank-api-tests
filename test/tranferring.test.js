f16ed5f4ff5cc4aace386503cfe10beae5db1f18
Marcos Machado <marcosevandro.mesm@gmail.com>
2026-06-17 20:51:15 -0300

    Created an authentication helper to get token

--- test/tranferring.test.js
+++ test/tranferring.test.js
@@ -1,20 +1,13 @@
 request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
+const { getToken } = require('../helpers/authentication');
 
 describe('Transfer', () => {
     describe('POST /transferencias', () => {
         it('Must return a 201 status code if the transferring value is equal or more than 10', async() => {
-            
-            const loginResponse = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'marcos.evandro',
-                    'senha': '123456'
-                })
 
-            const token = loginResponse.body.token; 
+            const token = await getToken('marcos.evandro', '123456');
 
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
@@ -31,15 +24,8 @@ describe('Transfer', () => {
     
         })
         it('Must return a 422 status code if the transferring value is less than or equal to 10', async() => {
-            const loginResponse = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'marcos.evandro',
-                    'senha': '123456'
-                })
 
-            const token = loginResponse.body.token; 
+            const token = await getToken('marcos.evandro', '123456');
 
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
