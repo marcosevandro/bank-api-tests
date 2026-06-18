8d089d80f937f3b6ea03dd2b508bcbc451570703
Marcos Machado <marcosevandro.mesm@gmail.com>
2026-06-17 21:47:50 -0300

    Added fixtures to manage the request bodies

--- test/login.test.js
+++ test/login.test.js
@@ -1,18 +1,18 @@
 request = require ('supertest');
 const { expect } = require ('chai');
 require('dotenv').config();
+const postLogin = require('../fixtures/postLogin.json')
 
 describe('Login', () => {
     describe('POST /login', () => {
         it('Must return 200 status code with a token as a string', async () => {
+            
+            const bodyLogin = {...postLogin}
+
             const response = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'marcos.evandro',
-                    'senha': '123456'
-
-            })
+                .send(bodyLogin)
             expect(response.status).to.equal(200);
             expect(response.body.token).to.be.a('string')
         })
--- test/tranferring.test.js
+++ test/tranferring.test.js
@@ -2,8 +2,9 @@ request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { getToken } = require('../helpers/authentication');
+const postTransferring = require('../fixtures/postTransferring.json')
 
-describe('Transfer', () => {
+describe('Transferring', () => {
     describe('POST /transferencias', () => {
 
         let token
@@ -14,32 +15,27 @@ describe('Transfer', () => {
 
         it('Must return a 201 status code if the transferring value is equal or more than 10', async() => {
 
+            const bodyTransferring = {...postTransferring}
+
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                        contaOrigem: 1,
-                        contaDestino: 2,
-                        valor: 11,
-                        token: ""
-                    })
+                .send(bodyTransferring)                    
 
             expect(response.status).to.equal(201)
     
         })
         it('Must return a 422 status code if the transferring value is less than or equal to 10', async() => {
+            
+            const bodyTransferring = {...postTransferring}
+            bodyTransferring.valor = 5
 
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                        contaOrigem: 1,
-                        contaDestino: 2,
-                        valor: 5,
-                        token: ""
-                    })
+                .send(bodyTransferring)                    
 
             expect(response.status).to.equal(422)
         })
